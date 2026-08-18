ce70945e2d6440cfe5b6775723253d5da92ffd98
bobbiyym93-boop <bobbiyym93@gmail.com>
2026-08-18 17:08:42 +0800

    Enable Chinese localization for density UI

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { densityChinesePlugin } from './density-localize-plugin.js'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [densityChinesePlugin(), vue()],
   server: {
     proxy: {
       '/api': {
